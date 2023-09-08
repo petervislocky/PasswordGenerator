e855e7d88692009125db70a2de73cb46be3178e0
Peter Vislocky <lilv447@gmail.com>
2023-09-08 03:57:12 -0400

    initial commit for this repository, deleted old one and started fresh

---
+++ src/com/passwdGen.java
@@ -0,0 +1,177 @@
+package com;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class passwdGen {
+	
+	private final String upperLettersKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private final String lowerLettersKey = "abcdefghijklmnopqrstuvwxyz";
+	private final String numsKey = "1234567890";
+	private final String symbolsKey = "!@#$%^&*?";
+	
+	private boolean runagain = true;
+	
+	private Scanner input;
+	
+	public passwdGen(Scanner scanner) {
+		this.input = scanner;
+		mainloop();
+	}
+	
+	public void mainloop() {
+		boolean upperLetters = true;
+		boolean lowerLetters = true;
+		boolean nums = true;
+		boolean symbols = true;
+		
+			while (runagain) {
+				System.out.println("Password Generator");
+				
+				int length = returnLength();
+				try {
+					System.out.print("Upper case letters included (y/n): ");
+					String upperCase_Included = input.nextLine();
+					
+					System.out.print("Lower case letters included (y/n): ");
+					String lowerCase_Included = input.nextLine();
+					
+					System.out.print("Numbers included (y/n): ");
+					String nums_Included = input.nextLine();
+					
+					System.out.print("Symbols included (y/n): ");
+					String symbols_Included = input.nextLine();
+					if (upperCase_Included.equalsIgnoreCase("y")) {
+						upperLetters = true;
+					}
+					else if (upperCase_Included.equalsIgnoreCase("n")) {
+						upperLetters = false;
+					} else {
+						System.out.println("Invalid input given for upper case letters. Returning true");
+						upperLetters = true;
+					}
+					
+					if (lowerCase_Included.equalsIgnoreCase("y")) {
+						lowerLetters = true;
+					}
+					else if (lowerCase_Included.equalsIgnoreCase("n")) {
+						lowerLetters = false;
+					} else {
+						System.out.println("Invalid input given for lower case letters. Returning true");
+						lowerLetters = true;
+					}
+					
+					if (nums_Included.equalsIgnoreCase("y")) {
+						nums = true;
+					}
+					else if (nums_Included.equalsIgnoreCase("n")) {
+						nums = false;
+					} else {
+						System.out.println("Invalid input given for numbers. Returning true");
+						nums = false;
+					}
+					
+					if (symbols_Included.equalsIgnoreCase("y")) {
+						symbols = true;
+					}
+					else if (symbols_Included.equalsIgnoreCase("n")) {
+						symbols = false;
+					} else {
+						System.out.println("Invalid input given for symbols. Returning true");
+						symbols = true;
+					}
+				}
+				catch (InputMismatchException e) {
+					System.out.println("Invalid input\nError code: " + e);
+					continue;
+				}
+				String password = generator(length, upperLetters, lowerLetters, nums, symbols);
+				//add in if statement to prevent an empty string printed when the user says no to all prompts
+				System.out.println("Generated Password: " + password);
+				runagain = runAgain();
+			}
+	}
+	
+	public String generator(int length, boolean upperLetters, boolean lowerLetters, boolean nums, boolean symbols) {
+		
+		Random random = new Random();
+		StringBuilder password = new StringBuilder();
+		
+		if (upperLetters) {
+			password.append(this.upperLettersKey);
+		}
+		if (lowerLetters) {
+			password.append(this.lowerLettersKey);
+		}
+		if (nums) {
+			password.append(this.numsKey);
+		}
+		if (symbols) {
+			password.append(this.symbolsKey);
+		}
+		
+		StringBuilder generatedPassword = new StringBuilder();
+		try {
+			for (int i = 0; i < length; i++) {
+				int index = random.nextInt(password.length());
+				char randomChar = password.charAt(index);
+				generatedPassword.append(randomChar);
+			}
+		}
+		catch (IllegalArgumentException e) {
+			System.out.println("Invalid input, must select desired characters. ");
+		}
+		
+		return generatedPassword.toString();
+	}
+	
+	public boolean runAgain() {
+		boolean RAloop = true;
+		boolean runAgain_returnValue = true;
+		while (RAloop) {
+			System.out.print("Run again? (y/n): ");
+			String again = input.nextLine();
+			if (again.equalsIgnoreCase("y")) {
+				RAloop = false;
+				runAgain_returnValue = true;
+			}
+			else if (again.equalsIgnoreCase("n")) {
+				RAloop = false;
+				runAgain_returnValue = false;
+			} else {
+				System.out.println("Invalid input");
+				
+			}
+		}
+		return runAgain_returnValue;
+	}
+	
+	public int returnLength() {
+		int length = 0;
+		boolean lengthLoop = true;
+		while (lengthLoop) {
+				System.out.print("Desired length (Min 1, Max 99): ");
+				try {
+					length = input.nextInt();
+					input.nextLine();
+					if (length > 99) {
+						System.out.println("Maximum length is 99");
+					}
+					else if (length < 1) {
+						System.out.println("Minimum length is 1");
+					} else {
+						lengthLoop = false;
+						return length;
+					}
+					
+				} 
+				catch (InputMismatchException e) {
+					System.out.println("Invalid input. You either entered invalid characters or a number too large\nRemember the minimum length is 1, maximum is 99");
+					input.next();
+				}
+		}
+		return length;
+	}
+}
+
---
+++ src/com/passwdGenMain.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.util.Scanner;
+
+public class passwdGenMain {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		passwdGen passGen = new passwdGen(scanner);
+	}
+
+}
