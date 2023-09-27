64d642a4d2acfbf967e9e6e82d953e92e85aa980
Peter Vislocky <lilv447@gmail.com>
2023-09-27 04:17:04 -0400

    Built the first two options in the menu method, have not tested them yet. Original program still works as normal as menu method has not been implemented yet

--- src/com/passwdGen.java
+++ src/com/passwdGen.java
@@ -22,6 +22,7 @@ public class passwdGen {
 	 * TODO create a menu method listing all different options for user and calling a different method depending on what the user selects
 	 * TODO add in multi-language support, look for a translator API
 	 */
+	private PasswordSettings settings = new PasswordSettings();
 	
 	public passwdGen(Scanner scanner) {
 		this.input = scanner;
@@ -35,15 +36,13 @@ public class passwdGen {
 		boolean symbols;
 		
 			while (runagain) {
-				boolean copyToClip = true;
-				
 				System.out.println("Password Generator\nBe aware, improper input for any of the prompts asking what characters you want included will default to yes.\nNone of the inputs are case sensitive.");
-				
+				//all code between here and the runAgain() method call is to be deleted and replaced with the menu method call when I'm done writing that.
+				boolean copyToClip = true;
 				UserInputHandler UIHand = new UserInputHandler(input);
+				
 				int length = UIHand.returnLength();
 				
-				//the following is a test of the Password settings class
-				PasswordSettings settings = new PasswordSettings();
 				upperLetters = UIHand.getCharChoice("Upper case letters included? ");
 				lowerLetters = UIHand.getCharChoice("Lower letters included? ");
 				nums = UIHand.getCharChoice("Numbers included? ");
@@ -57,7 +56,7 @@ public class passwdGen {
 				boolean numsIncluded = settings.getProperty("nums");
 				boolean symbolsIncluded = settings.getProperty("symbols");
 				settings.saveProperties();
-				//end of test, this part will be in a settings menu and won't prompt the user every time once implemented properly
+				
 				String password = generator(length, upperIncluded, lowerIncluded, numsIncluded, symbolsIncluded);
 				if (password.isEmpty()) {
 					runagain = UIHand.runAgain();
@@ -78,21 +77,91 @@ public class passwdGen {
 							continue;
 						}
 					}
+					//stop deleting here
 					runagain = UIHand.runAgain();
 				}
 			}
 	}
-	/*
+	//untested work in progress, not in use at the moment
 	public void menu() {
+		boolean upperLetters;
+		boolean lowerLetters;
+		boolean nums;
+		boolean symbols;
+		boolean genLoop = true;
+		boolean settingsLoop = true;
+		
+		UserInputHandler UIHand = new UserInputHandler(input);
+		
 		System.out.print("Choose an option by inputting the corresponding number\n1 Password Generator \n2Settings \n3 Useful Information \n4 Instructions for Use \n5 Exit");
 		int menuChoice = input.nextInt();
 		input.nextLine();
 		switch (menuChoice) {
-		case 1: 
-			
+		case 1:
+			while (genLoop) {
+				boolean copyToClip = true;
+				int length = UIHand.returnLength();
+				System.out.println("Generating Password ");
+				boolean upperIncluded = settings.getProperty("upperLetters");
+				boolean lowerIncluded = settings.getProperty("lowerLetters");
+				boolean numsIncluded = settings.getProperty("nums");
+				boolean symbolsIncluded = settings.getProperty("symbols");
+				String password = generator(length, upperIncluded, lowerIncluded, numsIncluded, symbolsIncluded);
+				if (password.isEmpty()) {
+					genLoop = UIHand.runAgain();
+				} else {
+					System.out.println("\u001B[92mGenerated Password: " + password + "\u001B[0m");
+				}
+				while(copyToClip) {
+					System.out.print("Copy password to clipboard? (y/n): ");
+					String copy = input.nextLine();
+					if (copy.equalsIgnoreCase("y")){
+						UIHand.copyToClipboard(password);
+						break;
+					}
+					else if (copy.equalsIgnoreCase("n")) {
+						break;
+					} else {
+						System.out.println("Invalid input. Correct input: (y/n)");
+						continue;
+					}
+				}
+				genLoop = UIHand.runAgain();
+			}
+			break;
+		case 2:
+			System.out.println("Settings\nChoose which characters to include, once you are done it will automatically save.");
+			while(settingsLoop) {
+				boolean confSettingsLoop = true;
+				upperLetters = UIHand.getCharChoice("Upper case letters included? ");
+				lowerLetters = UIHand.getCharChoice("Lower letters included? ");
+				nums = UIHand.getCharChoice("Numbers included? ");
+				symbols = UIHand.getCharChoice("Symbols included? ");
+				settings.setProperty("upperLetters", String.valueOf(upperLetters));
+				settings.setProperty("lowerLetters", String.valueOf(lowerLetters));
+				settings.setProperty("nums", String.valueOf(nums));
+				settings.setProperty("symbols", String.valueOf(symbols));
+				while(confSettingsLoop) {
+					System.out.println("Confirm settings? (y/n) ");
+					String confirmSettings = input.nextLine();
+					if (confirmSettings.equalsIgnoreCase("y")) {
+						settingsLoop = false;
+						confSettingsLoop = false;
+						menu();
+					}
+					else if (confirmSettings.equalsIgnoreCase("n")) {
+						settingsLoop = true;
+						confSettingsLoop = false;
+					} else {
+						System.out.println("Invalid input. Correct input: (y/n)");
+						confSettingsLoop = true;
+						
+					}
+				}
+			}
 		}
 	}
-	*/
+	
 	/*generates the password using the SecureRandom class and the StringBuilder class. Randomly selects an index from the character strings defined up top.
 	 *@param length 	  The desired length of the password
 	 *@param upperLetters Whether to include upper case letters
